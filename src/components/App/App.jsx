cf5faa284167620ea697482ec97f153c74f46da6
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-07-05 12:10:01 -0500

    Using this folder as a reference for full-stack GET and POST with React.js.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,78 @@
-import {useState} from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App () {
- 
+function App() {
   const [creatureList, setCreatureList] = useState([
-    {name :'Unicorn', origin: 'Britain'},
-    {name : 'Sphinx', origin: 'Egypt'},
-    {name: 'Jackalope', origin: 'America'}
+    { name: "Unicorn", origin: "Britain" },
+    { name: "Sphinx", origin: "Egypt" },
+    { name: "Jackalope", origin: "America" },
   ]);
-  
+
+  const [creatureNameInput, setCreatureNameInput] = useState('');
+  const [creatureOriginInput, setCreatureOriginInput] = useState('');
+
+
+  const fetchCreatures = () => {
+    /*  Alternative syntax for Axios call:
+  axios.get('/creature')
+  .then(...)
+   */
+
+    axios({
+      method: "GET",
+      url: "/creature",
+    })
+      .then((response) => {
+        // Response.data is where the server response data we care about is
+        console.log("Response is:", response.data);
+        setCreatureList(response.data);
+      })
+      .catch((error) => {
+        console.log("Error with fetch creatures:", error);
+        alert("Error fetching creatures.");
+      });
+  };
+
+  // Hook into the React life cycle and run fetchCreatures when this component loads
+  // React equivalent to JQuery's onReady function
+  useEffect(() => {
+    fetchCreatures();
+  }, []);
+  // 👆The empty array is saying that this should only run once when component loads
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Creature submitted!");
+
+    axios({
+      method: "POST",
+      url: "/creature",
+      data: {
+        name: creatureNameInput,
+        origin: creatureOriginInput
+      },
+    }).then((response) => {
+      console.log("Creature sent! Response is:", response);
+      fetchCreatures();
+      setCreatureNameInput("");
+      setCreatureOriginInput("");
+    }).catch((error) => {
+      console.log("Error with POST creature", error);
+      alert("Error with adding creature!");
+    })
+  }
+
   return (
     <div>
+      <form>
+        <input placeholder="Creature name" onChange={(event) => setCreatureNameInput(event.target.value)} value={creatureNameInput} />
+        <input placeholder="Creature origin" onChange={(event) => setCreatureOriginInput(event.target.value)} value={creatureOriginInput} />
+        <button onClick={handleSubmit} >Add New Creature</button>
+      </form>
+
+
       <ul>
-        {creatureList.map(creature => (
+        {creatureList.map((creature) => (
           <li key={creature.name}>
             {creature.name} is from {creature.origin}
           </li>
@@ -19,7 +80,6 @@ function App () {
       </ul>
     </div>
   );
-
 }
 
-export default App
+export default App;
